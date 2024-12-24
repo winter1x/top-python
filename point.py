e3f7a489a2c6d4579cbf8b03fc4bf458fefc94f0
tsenturion <80580892+tsenturion@users.noreply.github.com>
2024-12-24 20:16:14 +0500

    magic

--- point.py
+++ point.py
@@ -27,8 +27,30 @@ class Point:
         print(f'setting y {value}')
         self._y = value
 
+    def point_print(self):
+        print(f'Point x={self._x}, y={self._y}')
+
+    def __str__(self):
+        return f'Point x={self._x}, y={self._y}'
+
+    def __add__(self, other):
+        if not isinstance(other, Point):
+            return False
+        return Point(self._x + other._x, self._y + other._y)
+
+    def __eq__(self, other):
+        if not isinstance(other, Point):
+            return False
+        return self._x == other._x and self._y == other._y
+
 p1 = Point(1, 2)
 print(p1.x)
 print(p1.y)
 p1.x = 5
 p1.y = 8
+p1.point_print()
+print(p1)
+p2 = Point(1, 2)
+p1 += p2
+print(p1)
+print(p1 == Point(6, 10))
