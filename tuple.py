dd86b59302b6e167d054a644cf17995c7263700d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2024-12-26 19:38:32 +0500

    1 2 3 pz

--- tuple.py
+++ tuple.py
@@ -1,4 +1,4 @@
-tup = (1, 2, 4, '123')
+"""tup = (1, 2, 4, '123')
 print(tup)
 print(type(tup).__name__)
 tup2 = ([], 1, 2, 4, '123')
@@ -13,4 +13,17 @@ print(tup + tup2)
 print(2 in tup)
 print(len(tup))
 for i in tup:
-    print(i)
+    print(i)"""
+
+"""to_search = input()
+data = ("banana", "apple", "bananamango", "mango", "strawberry-banana")
+amount = 0
+for e in data:
+    if e == to_search or e.find(to_search) > 0:
+        amount += 1
+print(amount)"""
+data = ("qwe", "qwer", "qwert", "qwerty", "qwer", "qwert", "qwerty", "qwerty")
+title = input()
+to_replace = input()
+result = tuple(map(lambda x : to_replace if x == title else x, data))
+print(result)
