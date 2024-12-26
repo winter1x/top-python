cb1a1e278f9a808b1c60b1f1e7d688a21118ec41
tsenturion <80580892+tsenturion@users.noreply.github.com>
2024-12-26 20:05:53 +0500

    set

---
+++ задания/26.12/set.py
@@ -0,0 +1,40 @@
+inp = [1, 1, 2, 2]
+print(set(inp))
+some_set = {1, 2, 3, 3}
+print(some_set)
+zero2_set = set()
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3)
+zero2_set.add(3) # добавление
+print(zero2_set)
+zero2_set.update(range(5))
+print(zero2_set)
+zero2_set.discard(1)
+print(zero2_set)
+zero2_set.remove(2)
+print(zero2_set)
+popped = zero2_set.pop()
+print(zero2_set)
+print(popped)
+zero2_set.clear()
+print(zero2_set)
+union_set = {1, 2, 3}.union({13, 2})
+union2_set = {1, 2, 3} | {13, 2}
+print(union_set)
+
+intersection_set = {1, 2, 3}.intersection({13, 2})
+intersection2_set = {1, 2, 3} & {13, 2}
+print(intersection_set)
+
+difference_set = {1, 2, 3}.difference({3, 4, 5})
+difference2_set = {1, 2, 3} - {3, 4, 5}
+print(difference_set)
+print(difference2_set)
