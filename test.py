228979d0fc2dafc09c5501b214bb3f510f33d224
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-09 20:16:35 +0500

    pz 5

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-s = input()
-result = ''
-for i in range(len(s)):
-    if i % 3 != 0:
-        result += s[i]
-print(result)
+import math
+
+
+numbers = []
+while True:
+    num = int(input())
+    if num == 0:
+        break
+    numbers.append(num)
+n = len(numbers)
+s = sum(numbers) / n
+sum_of_squares = sum((x - s) ** 2 for x in numbers)
+sigma = math.sqrt(sum_of_squares / (n - 1))
+print(sigma)
