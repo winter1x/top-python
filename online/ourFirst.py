0b85ce93ca66a5e758ab1edcb2f5c339cc362d23
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-11 14:38:08 +0500

    theory + pz

--- online/ourFirst.py
+++ online/ourFirst.py
@@ -1,1 +1,60 @@
-print(1, 2, 3, 4, '123' + '123', 5 % 2, 5 // 2)
+from time import *
+print(pow(2, 3))
+from math import pow
+
+"""print(1, 2, 3, 4, '123' + '123', 5 % 2, 5 // 2, 2 ** 3)
+number = input()  # ввод str с консоли
+number = bool(number)
+print(number)  # вывод в консоль
+"""
+"""
+str   строки "123" '123'
+int   целые числа 123 34554435435 35 53 534 53 334 345 53 453 345 43
+float дробные 123.3 5.4 6.0
+bool  булевы True/1 False/0
+
+перевод в другое тип
+str()
+int()
+float()
+bool()
+
+>
+<
+>=
+<=
+==
+!=
+and и/умножение
+or или/сложение
+not отрицание
+"""
+"""flag = 5 > 2 and 4 < 3 or True
+
+if 5 > 2:
+    print("правда")
+else:
+    print("ложь")
+
+if flag:
+    print("правда")
+else:
+    print("ложь")
+    """
+"""if False:
+    print()
+elif False:
+    print()
+elif False:
+    print()
+
+if True:
+    print()
+start = time()
+end = time()
+while end - start < 1:
+    end = time()
+    print(1)
+else:
+    print('цикл закончился')
+"""
