1e3889bdd169452f6ee739db544393eb817bbec2
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-16 19:14:37 +0500

    leetcode + index 2

--- offline/test.py
+++ offline/test.py
@@ -1,3 +1,7 @@
-list1 = [1]
-list1.pop(0)
+list1 = [1, 2, 3, 4, 5]
+for i in range(len(list1)):
+    if i % 2 == 0 and i != 0:
+        print(list1[i])
+list1 = [list1[i] for i in range(len(list1))
+         if i % 2 == 0 and i != 0]
 print(list1)
