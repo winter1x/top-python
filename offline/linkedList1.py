4890834d44d8e3775966f17f2fd54de9d8804b85
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-23 19:40:18 +0500

    task 2

--- offline/linkedList1.py
+++ offline/linkedList1.py
@@ -7,6 +7,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
     def add_to_tail(self, data):
         new_node = Node(data)
         if not self.head:
@@ -18,7 +19,12 @@ class LinkedList:
         last.next = new_node
 
     def __str__(self):
-        pass
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.data))
+            current = current.next
+        return ' -> '.join(result) + ' -> None'
 
     def print_list(self):
         current = self.head
@@ -26,12 +32,77 @@ class LinkedList:
             print(current.data, end=' -> ')
             current = current.next
         print('None')
-
+    def remove_from_head(self):
+        """
+        удаление с головы
+        :return:
+        """
+        pass
+    def remove_by_value(self):
+        """
+        удаление по значению
+        :return:
+        """
+        pass
+    def find(self):
+        """
+        поиск элемента
+        :return:
+        """
+        pass
+    def length(self):
+        """
+        длина списка
+        :return:
+        """
+        pass
+    def insert_after(self):
+        """
+        вставка после элемента
+        :return:
+        """
+        pass
+    def reverse(self):
+        """
+        перевернуть
+        :return:
+        """
+        pass
+    def clear(self):
+        """
+        полная очистка
+        :return:
+        """
+        pass
+    def get_at_index(self):
+        """
+        получить по индексу
+        :return:
+        """
+        pass
+    def remove_at_index(self):
+        """
+        удалить по индексу
+        :return:
+        """
+        pass
+    def is_empty(self):
+        """
+        проверка на пустоту
+        :return:
+        """
+        pass
+    def merge(self):
+        """
+        увеличнение на список
+        :return:
+        """
+        pass
 
 li = LinkedList()
 li.print_list()
 li.add_to_tail(10)
 li.add_to_tail(20)
 li.add_to_tail(30)
-
+print(li)
 li.print_list()
