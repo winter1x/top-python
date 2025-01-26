54c031b940f7039137e72f26ea721aa745de7a2c
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-26 14:13:35 +0500

    nation asia

--- files.py
+++ files.py
@@ -1,4 +1,5 @@
 reg = -1
+nat = []
 with open('txts/region.txt', 'r') as file:
     lines = file.readlines()
     to_find = 'ASIA'
@@ -11,7 +12,11 @@ with open('txts/region.txt', 'r') as file:
         if line.find(to_find) > 0:
             reg2 = line[0]
             break"""
+with open('txts/nation.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        if reg in line.split('|')[2]:
+            nat.append(line.split('|')[0])
+print(nat)
 with open('txts/customer.txt', 'r') as file:
     lines = file.readlines()
-    # content = file.read()
-    print(lines[0])
