da2ac4fc2635e61831aa2c7a0233a3cc564edadd
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-26 13:25:01 +0500

    pz reduce

--- offline/test.py
+++ offline/test.py
@@ -1,15 +1,34 @@
+from functools import reduce
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+strs = ['123', '456']
+
 # Найдите сумму всех элементов списка чисел.
+sum_result = reduce(lambda x, y : x + y, numbers)
+print(sum_result)
 
 # Найдите произведение всех элементов списка чисел.
+sum_result = reduce(lambda x, y : x * y, numbers)
+print(sum_result)
 
 # Найдите максимальный элемент в списке чисел.
+sum_result = reduce(lambda x, y : x if x > y else y, numbers)
+print(sum_result)
 
 # Объедините все строки из списка в одну строку через пробел.
+conc_result = reduce(lambda x, y : x + ' ' + y, strs)
+print(conc_result)
 
 # Найдите сумму квадратов всех элементов списка чисел.
-
-# Вычислите факториал числа, используя список чисел от 1 до этого числа.
+sum_result = reduce(lambda x, y : x + y ** 2, numbers)
+print(sum_result)
 
 # Найдите наименьший элемент в списке чисел.
+sum_result = reduce(lambda x, y : x if x < y else y, numbers)
+print(sum_result)
+
+# Вычислите факториал числа, используя список чисел от 1 до этого числа.
+n = 5
+
 
 # Подсчитайте количество чётных чисел в списке, используя reduce.
