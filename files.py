aac7400cf8e416f60f25eda48a1c7fdf8c58d408
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-26 14:02:51 +0500

    region asia

--- files.py
+++ files.py
@@ -1,8 +1,16 @@
+reg = -1
 with open('txts/region.txt', 'r') as file:
     lines = file.readlines()
     to_find = 'ASIA'
     # content = file.read()
-    print(lines[0])
+    for line in lines:
+        if to_find in map(lambda s : s.strip(), line.split('|')):
+            reg = line[0]
+            break
+    """for line in lines:
+        if line.find(to_find) > 0:
+            reg2 = line[0]
+            break"""
 with open('txts/customer.txt', 'r') as file:
     lines = file.readlines()
     # content = file.read()
