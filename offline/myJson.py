3df2a755de14e2a7cecc9e807e2df21555e35f2d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-01-28 19:50:45 +0500

    third json

--- offline/myJson.py
+++ offline/myJson.py
@@ -9,6 +9,10 @@ dump - сериализация dict в .json и запись в файл
 loads - десериализация .json в dict
 load - десериализация .json из файла в dict
 """
-#test_json_data = dumps(test_data_learn, indent=4, sort_keys=True, ensure_ascii=False)
-with open('test_json_data.json', 'w', encoding='utf-8') as file:
-    dump(test_data_learn, file, indent=4)
+test_json_data = dumps(test_data_learn, indent=4, sort_keys=True, ensure_ascii=False)
+"""with open('test_json_data.json', 'w', encoding='utf-8') as file:
+    dump(test_data_learn, file, indent=4)"""
+data = loads(test_json_data)
+with open('test_json_data.json', 'r', encoding='utf-8') as file:
+    data2 = load(file)
+    print(data2)
