846188aef86f79f18b312ad63229a7b76a3c6575
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-06 21:44:35 +0500

    theory def

--- 06.02.py
+++ 06.02.py
@@ -1,1 +1,12 @@
-print(1)
+def add(a, b):
+    return a + b
+
+def custom_print():
+    print('Hello, world!')
+
+def custom_print_amount(amount):
+    for _ in range(amount):
+        print('Hello, world!')
+
+def double_return():
+    return 2, 3
