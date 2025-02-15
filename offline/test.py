b191c1b9279b3bff1792d4859be576094b06c427
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-15 13:53:51 +0500

    pz1 done

--- offline/test.py
+++ offline/test.py
@@ -1,15 +1,40 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+import math
+
+class Square:
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def area(self):
+        return self.side_length ** 2
+
+    def perimeter(self):
+        return 4 * self.side_length
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
 
 
-if __name__ == '__main__':
+class CircleInSquare(Square, Circle):
+    def __init__(self, side_length):
+        Square.__init__(self, side_length)
+        Circle.__init__(self, side_length / 2)
 
-    app = QApplication(sys.argv)
+    def __str__(self):
+        return (
+            f"Квадрат, сторона = {self.side_length}, "
+            f"площадь = {self.area()}, "
+            f"периметр = {self.perimeter()}\n"
+            f"Окружность, радиус = {self.radius}, "
+            f"площадь = {self.area()}, "
+            f"длина окружности = {self.circumference():.2f}"
+        )
 
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-
-    sys.exit(app.exec_())
+shape = CircleInSquare(10)
+print(shape)
