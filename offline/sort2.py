3eeffb955125cdb0363ac74586377a3c1fec91c3
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-02-20 19:52:52 +0500

    sort

--- offline/sort2.py
+++ offline/sort2.py
@@ -1,39 +1,38 @@
-# 8 7 6 5 4 3 2 1
-# 1 2 3 4 5 6 7 8
-
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
+
     mid = len(arr) // 2
-    left_half =arr[:mid]
-    right_half = arr[mid:]
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
 
-    merge_sort(left_half)
-    merge_sort(right_half)
+    return merge(left_half, right_half)
 
-    i = j = k = 0
 
-    while i < len(left_half) and j < len(right_half) and k < len(arr):
-        if left_half[i] < right_half[j]:
-            arr[k] = left_half[i]
+def merge(left, right):
+    sorted_array = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
             i += 1
         else:
-            arr[k] = right_half[i]
+            sorted_array.append(right[j])
             j += 1
-        k += 1
 
-    while i < len(left_half):
-        arr[k] = left_half[i]
+    while i < len(left):
+        sorted_array.append(left[i])
         i += 1
-        k += 1
 
-    while i < len(right_half):
-        arr[k] = right_half[i]
-        i += 1
-        k += 1
+    while j < len(right):
+        sorted_array.append(right[j])
+        j += 1
 
+    return sorted_array
 
 
 arr = [64, 34, 25, 12, 22, 11, 90]
-merge_sort(arr)
-print(arr)
+
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
