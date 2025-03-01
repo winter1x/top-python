6e95dbd9419b573ba2c09fcc76d9f1856253148c
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-01 12:20:17 +0500

    pz1 50%

--- offline/test.py
+++ offline/test.py
@@ -1,62 +1,38 @@
 from dataclasses import dataclass
-from math import gcd
+from functools import total_ordering
 
-
+@total_ordering
 @dataclass
-class  Fraction:
-    numerator: int
-    denominator: int
+class Library:
+    name: str
+    address: str
+    books_count: int
 
-    def __post_init__(self):
-        if self.denominator == 0:
-            raise ZeroDivisionError("")
-        self._reduce()
+    def __add__(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError("")
+        return Library(self.name, self.address, self.books_count + value)
 
-    def _reduce(self):
-        """сокращает дробь"""
-        common_divisor = gcd(self.numerator, self.denominator)
-        self.numerator //= common_divisor
-        self.denominator //= common_divisor
-        if self.denominator < 0:
-            self.numerator = -self.numerator
-            self.denominator = -self.denominator
+    def __iadd__(self, value: int):
+        if not isinstance(value, int):
+            raise ValueError("")
+        self.books_count += value
+        return self
 
-
-    def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
-
-    def __add__(self, other):
-        if isinstance(other, Fraction):
-            new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
-            new_denominator = self.denominator * other.denominator
-
-            return Fraction(new_numerator, new_denominator)
-        return NotImplemented
-
+    #-
     def __sub__(self, other):
-        if isinstance(other, Fraction):
-            new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
-            new_denominator = self.denominator * other.denominator
+        pass
 
-            return Fraction(new_numerator, new_denominator)
-        return NotImplemented
+    # -=
+    def __isub__(self, other):
+        pass
 
-    def __mul__(self, other):
-        if isinstance(other, Fraction):
-            return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
-        return NotImplemented
+    def __eq__(self, other):
+        if not isinstance(other, Library):
+            raise NotImplemented("")
+        return self.books_count == other.books_count
 
-    def __truediv__(self, other):
-        if isinstance(other, Fraction):
-            if other.numerator == 0:
-                raise ZeroDivisionError("")
-            return Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
-        return NotImplemented
+    #<
+    def __lt__(self, other):
+        pass
 
-
-frac1 = Fraction(1, 2)
-frac2 = Fraction(3, 4)
-print(frac1 * frac2)
-print(frac1 / frac2)
-print(frac1 - frac2)
-print(frac1 + frac2)
