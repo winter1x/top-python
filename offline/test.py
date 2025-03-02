3093326161d8a803d5dfa7ad38b94ec6f00b5ce7
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-02 13:35:54 +0500

    pz o done

--- offline/test.py
+++ offline/test.py
@@ -9,3 +9,34 @@ class DiscountCalculator:
             return order_amount * 0.02  # 2% скидка
         else:
             return 0  # Без скидки
+
+#o
+from abc import ABC, abstractmethod
+
+class DiscountStrategy(ABC):
+    @abstractmethod
+    def calculate(self, order_amount):
+        pass
+
+class RegularCustomerDiscount(DiscountStrategy):
+    def calculate(self, order_amount):
+        return order_amount * 0.05
+
+class VIPCustomerDiscount(DiscountStrategy):
+    def calculate(self, order_amount):
+        return order_amount * 0.1
+
+class NewCustomerDiscount(DiscountStrategy):
+    def calculate(self, order_amount):
+        return order_amount * 0.02
+
+class NoDiscount(DiscountStrategy):
+    def calculate(self, order_amount):
+        return 0
+
+class DiscountCalculator:
+    def __init__(self, discount_strategy: DiscountStrategy):
+        self.discount_strategy = discount_strategy
+
+    def calculate_discount(self, order_amount):
+        return self.discount_strategy.calculate(order_amount)
