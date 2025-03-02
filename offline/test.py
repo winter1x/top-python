71415f8a009a52b3013cbc0902f7ec5e674edde6
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-02 14:07:21 +0500

    pz d

--- offline/test.py
+++ offline/test.py
@@ -1,56 +1,16 @@
-from abc import ABC, abstractmethod
-
-
-# Интерфейс работника
-class IWorker(ABC):
-    @abstractmethod
+class Worker:
     def work(self):
-        pass
-
-    @abstractmethod
-    def eat(self):
-        pass
+        print("Человек работает")
 
 
-# Обычный работник (человек)
-class Worker(IWorker):
-    def work(self):
-        print("Рабочий работает")
-
-    def eat(self):
-        print("Рабочий обедает")
-
-
-# Робот (не должен иметь метод eat)
-class RobotWorker(IWorker):
+class RobotWorker:
     def work(self):
         print("Робот работает")
 
-    def eat(self):
-        raise NotImplementedError("Робот не ест!")
 
-#i
-from abc import ABC, abstractmethod
+class WorkerService:
+    def __init__(self, worker: Worker):
+        self.worker = worker
 
-class IWorkable(ABC):
-    @abstractmethod
-    def work(self):
-        pass
-
-class IEatable(ABC):
-    @abstractmethod
-    def eat(self):
-        pass
-
-class Worker(IWorkable, IEatable):
-    def work(self):
-        print("Рабочий работает")
-
-    def eat(self):
-        print("Рабочий обедает")
-
-
-class RobotWorker(IWorkable):
-    def work(self):
-        print("Робот работает")
-
+    def manage(self):
+        self.worker.work()
