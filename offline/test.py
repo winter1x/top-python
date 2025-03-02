de39792b60b0724fc6e55f38e32a639f819c6dbc
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-02 13:49:56 +0500

    pz l done

--- offline/test.py
+++ offline/test.py
@@ -36,3 +36,37 @@ test_area(rect)  # Работает правильно
 
 sq = Square(4)
 test_area(sq)  # Ошибка! Ожидаем 50, но получаем 100
+
+
+#l
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def get_area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def set_side(self, side):
+        self.side = side
+
+    def get_area(self):
+        return self.side * self.side
