707f944ad75e65676355e50895a233024e9a9ab5
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-02 14:19:07 +0500

    pz d done

--- offline/test.py
+++ offline/test.py
@@ -14,3 +14,26 @@ class WorkerService:
 
     def manage(self):
         self.worker.work()
+
+#d
+from abc import ABC, abstractmethod
+
+class IWorkable(ABC):
+    @abstractmethod
+    def work(self):
+        pass
+
+class Worker(IWorkable):
+    def work(self):
+        print("Человек работает")
+
+class RobotWorker(IWorkable):
+    def work(self):
+        print("Робот работает")
+
+class WorkerService:
+    def __init__(self, worker: IWorkable):
+        self.worker = worker
+
+    def manage(self):
+        self.worker.work()
