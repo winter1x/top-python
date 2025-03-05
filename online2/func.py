42d42eb2e42695509fe7b47a38559a0dc9f5d63a
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-05 23:02:52 +0500

    функции

---
+++ online2/func.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def print_sum(a, b):
+    print(a + b)
+
+def some_func():
+    pass
+
+def some_func2():
+    print("some")
+
+def many_returns():
+    return 1, 2, 3, 4, 5
+
+print(add(1, 2))
+print_sum(1, 2)
+
+a, b, c, d, e = many_returns()
+print(a, b, c, d, e)
