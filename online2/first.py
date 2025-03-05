19ed3c2cc38521c0ce1356de2638106144cbc649
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-05 20:40:54 +0500

    while

--- online2/first.py
+++ online2/first.py
@@ -4,10 +4,10 @@ str - строки
 float - 1.1  плавающей (точкой)
 bool - True False
 """
-
+"""
 number = 10
 str1 = '112'
-str_number = "34"
+str_number = "34"""
 
 """num1 = input()
 print(num1 + 1)"""
@@ -43,12 +43,12 @@ finally:
 """print(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 average_grade = 1.111111111
 print(f"{average_grade:.2f}")"""
-average_grade = 1.111111111
+"""average_grade = 1.111111111
 
 #a = input()
 
 flag1 = True
-flag2 = False
+flag2 = False"""
 
 """
 >
@@ -65,10 +65,21 @@ not отрицание
 
 """boolean_value = 6 > 2 and False or flag1
 print(boolean_value)"""
-number = int(input())
+"""number = int(input())
 if number > 5:
     print(f"{number} > 5")
 elif number < 5:
     print(f"{number} < 5")
 else:
-    print(f"{number} = 5")
+    print(f"{number} = 5")"""
+count = 0
+while count < 10 and True:
+    if count == 5:
+        count += 1
+        continue
+    if count == 8:
+        break
+    print(count)
+    count += 1
+else:
+    print("цикл завершился")
