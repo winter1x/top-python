bbe15e3dffcd48944c2475612cd5e05272e1636d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-10 22:08:38 +0500

    all

--- online2/first.py
+++ online2/first.py
@@ -204,3 +204,27 @@ a = max(1, 2)"""
 print(7 // 2)
 print(4001 % 2 == 0)
 
+# Считать с консоли номер дня недели. Вывести буквенную интерпретацию
+day = int(input("введите день недели"))
+"""if day == 1:
+    print('monday')
+elif day == 2:
+    print('втроник')
+elif day == 2:  print('втроник')
+elif day==  2:  print('втроник')
+elif day == 2:
+    print('втроник')
+elif day == 2:
+    print('втроник')
+elif day == 2:
+    print('втроник')
+    """
+match day:
+    case 1: print("пн")
+    case 2: print("")
+    case 3: print("")
+    case 4: print("")
+    case 5: print("")
+    case 6: print("")
+    case 7: print("")
+    case _: print("нет")
--- online2/test.py
+++ online2/test.py
@@ -1,24 +1,25 @@
-# Считать с консоли номер дня недели. Вывести буквенную интерпретацию
-day = int(input("введите день недели"))
-"""if day == 1:
-    print('monday')
-elif day == 2:
-    print('втроник')
-elif day == 2:  print('втроник')
-elif day==  2:  print('втроник')
-elif day == 2:
-    print('втроник')
-elif day == 2:
-    print('втроник')
-elif day == 2:
-    print('втроник')
-    """
-match day:
-    case 1: print("пн")
-    case 2: print("")
-    case 3: print("")
-    case 4: print("")
-    case 5: print("")
-    case 6: print("")
-    case 7: print("")
-    case _: print("нет")
+list1 = [1, 2, 3, 4, 5]
+list2 = list(range(1, 6))
+
+list3 = []
+for i in range(1, 6):
+    list3.append(i)
+
+list4 = [i for i in range(1, 6)]
+
+list5 = list(i for i in range(1, 6))
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+
+list6 = [i for i in range(1, 60) if i % 2 == 0]
+list7 = [i for i in range(1, 60) if i % 2 == 0 if i % 7 == 0]
+
+list8 = [i if i % 2 == 0 else None for i in range(1, 60)]
+
+
+list9 = [[i + j for i in range(5)] for j in range(5)]
+print(list9)
