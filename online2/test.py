09ab374e10f2731e8c07bb4c122a35e3b56bcd1d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-12 22:24:50 +0500

    pz3 done

--- online2/test.py
+++ online2/test.py
@@ -59,7 +59,63 @@ print(count)
 Программа получает на вход восемь пар чисел, каждое число от 1 до 8 — координаты 8 ферзей. 
 Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
 """
+n = 8
+x = []
+y = []
+for i in range(n):
+    new_x, new_y = [int(s) for s in input().split()]
+    x.append(new_x)
+    y.append(new_y)
 
+correct = True
+for i in range(n):
+    for j in range(i + 1, n):
+        if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+            correct = False
+
+if correct:
+    print('NO')
+else:
+    print('YES')
+
+#--------------------------------------
+queens = []
+for _ in range(8):
+    x, y = map(int, input().split())
+    queens.append((x, y))
+attack = False
+for i in range(8):
+    for j in range(i + 1, 8):
+        x1, y1 = queens[i]
+        x2, y2 = queens[j]
+        if x1 == x2 or y1 == y2:
+            attack = True
+            break
+        if abs(x1 - x2) == abs(y2 - y1):
+            attack = True
+            break
+if attack:
+    print('YES')
+else:
+    print("NO")
+#--------------------------------------
+queens = []
+for _ in range(8):
+    x, y = map(int, input().split())
+    queens.append((x, y))
+
+found = False
+for i in range(8):
+    for j in range(i + 1, 8):
+        x1, y1 = queens[i]
+        x2, y2 = queens[j]
+        if x1 == x2 or y1 == y2 or abs(x1 - x2) == abs(y1 - y2):
+            found = True
+            break
+    if found:
+        break
+
+print("YES" if found else "NO")
 """
 Дана строка. Удалите из нее все символы, чьи индексы делятся на 3.
 """
