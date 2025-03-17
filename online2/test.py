c286fd69e697e7848c8685653dbdb42b8494f22a
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-17 22:19:59 +0500

    pz1

--- online2/test.py
+++ online2/test.py
@@ -7,48 +7,8 @@
 """
 
 """
-Задание №2 Пользователь вводит с клавиатуры три числа. 
-В зависимости от выбора пользователя программа выводит на экран максимум из трёх,
-минимум из трёх или среднеарифметическое трёх чисел.
- """
-
-"""from functools import reduce
-# numpy.mean - ср арифм
-lola = [1, 2, 3]
-print(reduce(max, lola))
-print(reduce(min, lola))
-lola2 = sum(lola) / len(lola)
-print(lola2)
-
-print(max(lola))
-print(min(lola))
-
-a1 = 5
-b1 = 4
-c1 = 3
-print(f'Cреднеарифметическоее число из {a1}, {b1}, {c1}, это', (a1 + b1 + c1) / 3)"""
-
-''' Задание №3 Пользователь вводит с клавиатуры количество метров. 
-В зависимости от выбора пользователя программа переводит метры в мили, дюймы или ярды.'''
-"""meters = (45000)
-miles = meters / 1609.34
-print(int(miles))
-inches = meters * 39.3701
-print(int(inches))
-yards = meters * 1.09361
-print(int(yards))
-
-#meters = int(input("количество метров"))
-choose = input("miles/inches/yards by typing that or 1/2/3")
-if choose == 'miles' or choose == '1':
-    miles = meters / 1609.34
-    print(int(miles))
-elif choose == 'inches' or choose == '2':
-    inches = meters * 39.3701
-    print(int(inches))
-elif choose == 'yards' or choose == '3':
-    yards = meters * 1.09361
-    print(int(yards))
-else:
-    print("некорректный ввод")"""
-
+получить текущее через datetime
+измерить время выполнения 
+вывести в удобном формате через time
+рассчитать разницу между двумя датами через datetime
+"""
