84319a3b1514ac9d7c2a486374579422f89948bd
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-19 21:44:47 +0500

    pz3

--- files.py
+++ files.py
@@ -1,8 +1,22 @@
+"""
+чтение
+read() - читает весь файл целиком
+readlines() - читает файл построчно
+read(n) - указанное количество символов
+
+запись
+write() - строку в файл
+writelines(lines)
+"""
+"""file = open('txts/region.txt', 'r')
+#код
+file.close()"""
+
 reg = -1
 nat = []
 amount = 0
 
-with open('txts/region.txt', 'w') as file:
+with open('txts/region.txt', 'r') as file:
     lines = file.readlines()
     print(lines)
 
