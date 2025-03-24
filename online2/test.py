4c2d64c4524c9b7533d63c1abe31377a29ee3435
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-24 22:38:27 +0500

    pz4

--- online2/test.py
+++ online2/test.py
@@ -1,23 +1,20 @@
-# с помощью timeit замерить выполнение fibonacci с cache и без cache
-import timeit
+#uuid
 
-def fibonnaci_no_cache(n):
-    if n <= 1:
-        return n
-    return fibonnaci_no_cache(n - 1) + fibonnaci_no_cache(n - 2)
+"""
+замыкание
 
-time = timeit.timeit(lambda: fibonnaci_no_cache(30), number=2)
-print(time)
+генерирует последовательные идентификаторы
+возвращает следующий уникальный идентификатор
+"""
 
+"""
+ID0
+ID1
+ID2
+"""
 
-from functools import lru_cache
-
-@lru_cache(maxsize=None)
-def fibonacci_with_cache(n):
-    if n <= 1:
-        return n
-    return fibonacci_with_cache(n - 1) + fibonacci_with_cache(n - 2)
-
-time2 = timeit.timeit(lambda: fibonacci_with_cache(30), number=2)
-print(time2)
-print(time2 / time)
+"""
+USER0
+USER1
+USER2
+"""
