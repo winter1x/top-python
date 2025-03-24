f7207c4ba1270ff4b98fb2c6c6ccc0b147ca070d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-24 23:03:44 +0500

    pz4 done

--- online2/test.py
+++ online2/test.py
@@ -18,3 +18,24 @@ USER0
 USER1
 USER2
 """
+
+def create_id_generator(prefix="ID"):
+    current_id = 0
+
+    def generate_id():
+        nonlocal current_id
+        id_value = f'{prefix}{current_id}'
+        current_id += 1
+        return id_value
+    return generate_id
+
+id_generator = create_id_generator()
+
+print(id_generator())
+print(id_generator())
+print(id_generator())
+
+custom_id_generator = create_id_generator('USER')
+print(custom_id_generator())
+print(custom_id_generator())
+print(custom_id_generator())
