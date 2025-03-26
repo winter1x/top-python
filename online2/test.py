7bb9aa8ee2e0acab70adc9c7e5fe5340be200807
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-26 21:23:51 +0500

    pz + test

--- online2/test.py
+++ online2/test.py
@@ -5,3 +5,24 @@ odd - нечетные
 positive - положительные
 negative - отрицательные
 """
+
+сигнатура
+    сигнатура (если без партикал)
+        если передано even
+            фильтруем четные, ретерн
+        если передано odd
+            фильтруем нечетные, ретерн
+
+test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_filter = filter_numbers("even")
+print(even_filter(test_list))
+
+odd_filter = filter_numbers("odd")
+print(odd_filter(test_list))
+
+positive_filter = filter_numbers("positive")
+print(positive_filter(test_list))
+
+negative_filter = filter_numbers("negative")
+print(negative_filter(test_list))
