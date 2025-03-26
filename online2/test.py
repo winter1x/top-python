8ec48d62cdb9d3c064250024d241e3e9c958b18a
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-26 23:04:01 +0500

    pz done

--- online2/test.py
+++ online2/test.py
@@ -17,3 +17,22 @@ output
 объединение двух списков чисел с использованием * и передадите в multiply_numbers
 объединение двух словарей с разными данными и передадите в format_person_info
 """
+
+def multiply_numbers(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+def format_person_info(**kwargs):
+    return f"Имя: {kwargs['name']}, Возраст: {kwargs['age']}, Город: {kwargs['city']}"
+
+numbers1 = [2, 3, 4]
+numbers2 = [5, 6]
+
+person1 = {'name': 'ivan', 'age':23}
+person2 = {'city': "moscow"}
+
+print(multiply_numbers(*numbers1, *numbers2))
+
+print(format_person_info(**person1, **person2))
