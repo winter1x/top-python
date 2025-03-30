1f73636f000e226dc15b81b7a989fcdb61e88580
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-30 13:14:05 +0500

    pz done

--- online/test.py
+++ online/test.py
@@ -9,9 +9,14 @@ DataProcessor
 """
 
 from abc import ABC, abstractmethod
+import csv
 import json
 
 class DataProcessor(ABC):
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+
     def process(self):
         data = self.load_data()
         process_data = self.process_data(data)
@@ -31,34 +36,53 @@ class DataProcessor(ABC):
 
 class CSVProcessor(DataProcessor):
     def load_data(self):
-        print("загрузка из CSV")
-        return ["apple,10", 'banana,5', 'orange,7']
+        print(f"загрузка из {self.input_file}")
+        with open(self.input_file, newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            return [row for row in reader]
 
     def process_data(self, data):
         print("обработка из CSV")
-        return [{"name": row.split(",")[0], "count": int(row.split(",")[1])} for row in data]
+        return [{"name": row[0], 'count': int(row[1])} for row in data]
 
     def save_data(self, data):
-        print("сохранение в json")
-        print(json.dumps(data, indent=4, ensure_ascii=False))
+        print(f"сохранение в {self.output_file}")
+        with open(self.output_file, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
 
 class TextProcessor(DataProcessor):
     def load_data(self):
-        print("загрузка txt")
-        return "hello world"
+        print(f"загрузка {self.input_file}")
+        with open(self.input_file, 'r', encoding='utf-8') as file:
+            return file.read()
 
     def process_data(self, data):
         print('обработка текста')
         return data.upper()
 
     def save_data(self, data):
-        print('сохранение')
-        print(data)
+        print(f'сохранение в {self.output_file}')
+        with open(self.output_file, 'w', encoding='utf-8') as file:
+            file.write(data)
 
-csv_processor = CSVProcessor()
+csv_processor = CSVProcessor('data.csv', "data.json")
 csv_processor.process()
 
 print("------------")
 
-text_processor = TextProcessor()
+text_processor = TextProcessor('text.txt', 'text_processed.txt')
 text_processor.process()
+
+"""
+data.csv
+
+apple,10
+banana,5
+orange,7
+"""
+"""
+text.txt
+
+ewveefv
+"""
