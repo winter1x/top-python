b2eb8746acd75b9361e5bc45594dcfe7b552b121
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-31 21:37:19 +0500

    pz done

--- online2/test.py
+++ online2/test.py
@@ -7,6 +7,15 @@ from functools import reduce
 найти сумму
 """
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+sum_of_cubes = reduce(
+    lambda x, y: x + y,
+    map(
+        lambda x: x ** 3,
+        filter(lambda x: x % 2 == 0, numbers)
+    )
+)
 """
 оставить те где длина >3 
 
