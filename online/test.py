33dd3e5913ebcff83fe7cf20e3ba462295b73967
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-31 21:17:21 +0500

    map filter reduce

--- online/test.py
+++ online/test.py
@@ -4,3 +4,10 @@ SmartHub - посредник
 Light и Thermostat - отправляют команды через посредника
 когда включается свет, температура автоматически понижается
 """
+
+from abc import ABC, abstractmethod
+
+class SmartHub(ABC):
+    @abstractmethod
+    def notify(self, sender, event):
+        pass
--- online2/test.py
+++ online2/test.py
@@ -1,38 +1,7 @@
-"""
-multiply_numbers
-- принимает произвольное колво чисел
-- возрвращет произведение
-"""
-"""
-format_person_info
-- принимает именованные name, age, city
-- return "имя <>..."
-"""
-"""
-список, передаем в multiply_numbers
-словарь с info о person, передать в format_person_info
-"""
-"""
-output
-объединение двух списков чисел с использованием * и передадите в multiply_numbers
-объединение двух словарей с разными данными и передадите в format_person_info
-"""
+from functools import reduce
 
-def multiply_numbers(*args):
-    result = 1
-    for num in args:
-        result *= num
-    return result
-
-def format_person_info(**kwargs):
-    return f"Имя: {kwargs['name']}, Возраст: {kwargs['age']}, Город: {kwargs['city']}"
-
-numbers1 = [2, 3, 4]
-numbers2 = [5, 6]
-
-person1 = {'name': 'ivan', 'age':23}
-person2 = {'city': "moscow"}
-
-print(multiply_numbers(*numbers1, *numbers2))
-
-print(format_person_info(**person1, **person2))
+"""
+оставить нечетные числа
+возвести их в куб
+найти сумму
+"""
