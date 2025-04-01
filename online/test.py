571677680dc6d85f4cd6b6157221446c3944ce82
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-01 20:25:42 +0500

    pz

--- online/test.py
+++ online/test.py
@@ -4,10 +4,57 @@ SmartHub - посредник
 Light и Thermostat - отправляют команды через посредника
 когда включается свет, температура автоматически понижается
 """
-
 from abc import ABC, abstractmethod
 
 class SmartHub(ABC):
     @abstractmethod
     def notify(self, sender, event):
         pass
+
+class HomeAutomationHub(SmartHub):
+    def __init__(self):
+        self.devices = {}
+
+    def register_device(self, device):
+        self.devices[device.name] = device
+
+    def notify(self, sender, event):
+        if sender == "Light" and event == "ON":
+            print("SmartHub: Свет включен. Понижаем температуру.")
+            if "Thermostat" in self.devices:
+                self.devices["Thermostat"].set_temperature(18)
+        elif sender == "Light" and event == "OFF":
+            print("SmartHub: Свет выключен. Температура возвращается к норме.")
+            if "Thermostat" in self.devices:
+                self.devices["Thermostat"].set_temperature(22)
+
+
+class SmartDevice:
+    def __init__(self, name, hub):
+        self.name = name
+        self.hub = hub
+        hub.register_device(self)
+
+
+class Light(SmartDevice):
+    def turn_on(self):
+        print("💡 Свет включен.")
+        self.hub.notify("Light", "ON")
+
+    def turn_off(self):
+        print("💡 Свет выключен.")
+        self.hub.notify("Light", "OFF")
+
+
+class Thermostat(SmartDevice):
+    def set_temperature(self, temperature):
+        print(f"🌡 Термостат установлен на {temperature}°C")
+
+
+hub = HomeAutomationHub()
+
+light = Light("Light", hub)
+thermostat = Thermostat("Thermostat", hub)
+
+light.turn_on()
+light.turn_off()
--- online2/test.py
+++ online2/test.py
@@ -8,3 +8,29 @@ dec - 1
 reset в 0
 stop
 """
+
+def smart_counter():
+    count = 0
+    while True:
+        command = yield count
+        if command == "inc":
+            count += 1
+        elif command == "dec":
+            count -= 1
+        elif command == "reset":
+            count = 0
+        elif command == "stop":
+            raise StopIteration("остановлен")
+
+counter = smart_counter()
+next(counter)
+
+
+
+
+try:
+    print(next(counter))
+    print(counter.send("inc"))
+    counter.send("stop")
+except StopIteration as e:
+    print(e)
