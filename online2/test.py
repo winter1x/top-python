3ea673cedeaadeb8edd67657f87638264351ef7e
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-02 23:10:18 +0500

    pzdone

--- online2/test.py
+++ online2/test.py
@@ -6,25 +6,44 @@
 добавить возможность выбора пользователем в каком порядке сортировать
 """
 
-def selection_sort(arr):
+def selection_sort(arr, ascending=True):
     n = len(arr)
     left, right = 0, n - 1
+    total_swaps = 0
+    total_iterations = 0
+
     while left < right:
         min_index = left
         max_index = right
 
         for i in range(left, right + 1):
-            if arr[i] < arr[min_index]:
+            if (ascending and arr[i] < arr[min_index]) or (not ascending and arr[i] > arr[min_index]):
                 min_index = i
-            if arr[i] > arr[min_index]:
+            if (ascending and arr[i] > arr[max_index]) or (not ascending and arr[i] < arr[max_index]):
                 max_index = i
 
-        arr[left], arr[min_index] = arr[min_index], arr[left]
+        if min_index != left:
+            arr[left], arr[min_index] = arr[min_index], arr[left]
+            total_swaps += 1
 
-        if max_index == left:
-            max_index = min_index
+            if max_index == left:
+                max_index = min_index
 
-        arr[right], arr[max_index] = arr[max_index], arr[right]
+        if max_index != right:
+            arr[right], arr[max_index] = arr[max_index], arr[right]
+            total_swaps += 1
 
         left += 1
         right -= 1
+        total_iterations += 1
+
+        print(f"шаг {total_iterations}: {arr}")
+
+    return total_iterations, total_swaps
+
+numbers = list(map(int, input().split()))
+order = input("asc - по возрастания / decs - по убыванию").strip().lower()
+ascending = True if order == 'asc' else False
+iterations, swaps = selection_sort(numbers, ascending)
+
+print(iterations, swaps)
