67c02a572aa78ec50bdf4d8e8497df5f4ff4b209
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-03 19:21:11 +0500

    Command

--- online/test.py
+++ online/test.py
@@ -1,60 +1,7 @@
 """
-mediator
-SmartHub - посредник
-Light и Thermostat - отправляют команды через посредника
-когда включается свет, температура автоматически понижается
-"""
-from abc import ABC, abstractmethod
+command
 
-class SmartHub(ABC):
-    @abstractmethod
-    def notify(self, sender, event):
-        pass
+история команд, чтобы можно было посмотреть последние 5 действий.
+несколько устройств телевизор, кондиционер
+комбинировация команд в макрос например, Включить всё  включает сразу несколько устройств
 
-class HomeAutomationHub(SmartHub):
-    def __init__(self):
-        self.devices = {}
-
-    def register_device(self, device):
-        self.devices[device.name] = device
-
-    def notify(self, sender, event):
-        if sender == "Light" and event == "ON":
-            print("SmartHub: Свет включен. Понижаем температуру.")
-            if "Thermostat" in self.devices:
-                self.devices["Thermostat"].set_temperature(18)
-        elif sender == "Light" and event == "OFF":
-            print("SmartHub: Свет выключен. Температура возвращается к норме.")
-            if "Thermostat" in self.devices:
-                self.devices["Thermostat"].set_temperature(22)
-
-
-class SmartDevice:
-    def __init__(self, name, hub):
-        self.name = name
-        self.hub = hub
-        hub.register_device(self)
-
-
-class Light(SmartDevice):
-    def turn_on(self):
-        print("💡 Свет включен.")
-        self.hub.notify("Light", "ON")
-
-    def turn_off(self):
-        print("💡 Свет выключен.")
-        self.hub.notify("Light", "OFF")
-
-
-class Thermostat(SmartDevice):
-    def set_temperature(self, temperature):
-        print(f"🌡 Термостат установлен на {temperature}°C")
-
-
-hub = HomeAutomationHub()
-
-light = Light("Light", hub)
-thermostat = Thermostat("Thermostat", hub)
-
-light.turn_on()
-light.turn_off()
