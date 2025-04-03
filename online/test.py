025b16cd9aaa3e21b7442a6691e9eba3ec545def
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-03 21:02:26 +0500

    iterator

--- online/test.py
+++ online/test.py
@@ -1,161 +1,41 @@
-"""
-command
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
 
-история команд, чтобы можно было посмотреть последние 5 действий.
-несколько устройств телевизор, кондиционер
-комбинировация команд в макрос например, Включить всё  включает сразу несколько устройств
+    def add_child(self, child_node):
+        self.children.append(child_node)
 
-"""
-from abc import ABC, abstractmethod
-from collections import deque
+    def __iter__(self):
+        return TreeIterator(self)
 
-class Command(ABC):
-    @abstractmethod
-    def execute(self):
-        pass
 
-    @abstractmethod
-    def undo(self):
-        pass
+class TreeIterator:
+    def __init__(self, root):
+        self.stack = [root]
 
-class Light:
-    def turn_on(self):
-        print("Свет включен")
+    def __iter__(self):
+        return self
 
-    def turn_off(self):
-        print("Свет выключен")
+    def __next__(self):
+        if not self.stack:
+            raise StopIteration
 
-class TV:
-    def turn_on(self):
-        print("Телевизор включен")
+        current_node = self.stack.pop(0)
+        self.stack.extend(current_node.children)
+        return current_node.value
 
-    def turn_off(self):
-        print("Телевизор выключен")
 
-class AC:
-    def turn_on(self):
-        print("Кондиционер включен")
+root = TreeNode("A")
+node_b = TreeNode("B")
+node_c = TreeNode("C")
+node_d = TreeNode("D")
+node_e = TreeNode("E")
 
-    def turn_off(self):
-        print("Кондиционер выключен")
+root.add_child(node_b)
+root.add_child(node_c)
+node_b.add_child(node_d)
+node_b.add_child(node_e)
 
-class LightOnCommand(Command):
-    def __init__(self, light):
-        self.light = light
-
-    def execute(self):
-        self.light.turn_on()
-
-    def undo(self):
-        self.light.turn_off()
-
-class LightOffCommand(Command):
-    def __init__(self, light):
-        self.light = light
-
-    def execute(self):
-        self.light.turn_off()
-
-    def undo(self):
-        self.light.turn_on()
-
-class TVOnCommand(Command):
-    def __init__(self, tv):
-        self.tv = tv
-
-    def execute(self):
-        self.tv.turn_on()
-
-    def undo(self):
-        self.tv.turn_off()
-
-class TVOffCommand(Command):
-    def __init__(self, tv):
-        self.tv = tv
-
-    def execute(self):
-        self.tv.turn_off()
-
-    def undo(self):
-        self.tv.turn_on()
-
-class ACOnCommand(Command):
-    def __init__(self, ac):
-        self.ac = ac
-
-    def execute(self):
-        self.ac.turn_on()
-
-    def undo(self):
-        self.ac.turn_off()
-
-class ACOffCommand(Command):
-    def __init__(self, ac):
-        self.ac = ac
-
-    def execute(self):
-        self.ac.turn_off()
-
-    def undo(self):
-        self.ac.turn_on()
-
-class MacroCommand(Command):
-    def __init__(self, commands):
-        self.commands = commands
-
-    def execute(self):
-        for command in self.commands:
-            command.execute()
-
-    def undo(self):
-        for command in reversed(self.commands):
-            command.undo()
-
-class RemoteControl:
-    def __init__(self):
-        self.command_history = deque(maxlen=5)
-
-    def press_button(self, command):
-        command.execute()
-        self.command_history.append(command)
-
-    def press_undo(self):
-        if self.command_history:
-            command = self.command_history.pop()
-            command.undo()
-        else:
-            print("История пуста, отменять нечего.")
-
-    def show_history(self):
-        print("\nИстория команд:")
-        for i, cmd in enumerate(self.command_history, 1):
-            print(f"{i}. {cmd.__class__.__name__}")
-
-light = Light()
-tv = TV()
-ac = AC()
-
-remote = RemoteControl()
-
-light_on = LightOnCommand(light)
-tv_on = TVOnCommand(tv)
-ac_on = ACOnCommand(ac)
-
-light_off = LightOffCommand(light)
-tv_off = TVOffCommand(tv)
-ac_off = ACOffCommand(ac)
-
-turn_on_all = MacroCommand([light_on, tv_on, ac_on])
-turn_off_all = MacroCommand([light_off, tv_off, ac_off])
-
-remote.press_button(light_on)
-remote.press_button(tv_on)
-remote.press_button(ac_on)
-
-remote.show_history()
-
-remote.press_undo()
-remote.show_history()
-
-remote.press_button(turn_on_all)
-remote.press_undo()
+for node_value in root:
+    print(node_value)
