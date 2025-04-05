b8842d8c9ea8206f50f993049ccc174d2a4de2e9
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-05 13:08:03 +0500

    command pz done

--- online/test.py
+++ online/test.py
@@ -7,8 +7,15 @@
 вкл все
 выкл все
 """
+"""
+command
 
+история команд, чтобы можно было посмотреть последние 5 действий.
+несколько устройств телевизор, кондиционер
+комбинировация команд в макрос например, Включить всё  включает сразу несколько устройств
+"""
 from abc import ABC, abstractmethod
+from collections import deque
 
 class Command(ABC):
     @abstractmethod
@@ -18,6 +25,27 @@ class Command(ABC):
     @abstractmethod
     def undo(self):
         pass
+
+class Light:
+    def turn_on(self):
+        print("Свет включен")
+
+    def turn_off(self):
+        print("Свет выключен")
+
+class TV:
+    def turn_on(self):
+        print("Телевизор включен")
+
+    def turn_off(self):
+        print("Телевизор выключен")
+
+class AC:
+    def turn_on(self):
+        print("Кондиционер включен")
+
+    def turn_off(self):
+        print("Кондиционер выключен")
 
 class LightOnCommand(Command):
     def __init__(self, light):
@@ -39,28 +67,103 @@ class LightOffCommand(Command):
     def undo(self):
         self.light.turn_on()
 
+class TVOnCommand(Command):
+    def __init__(self, tv):
+        self.tv = tv
+
+    def execute(self):
+        self.tv.turn_on()
+
+    def undo(self):
+        self.tv.turn_off()
+
+class TVOffCommand(Command):
+    def __init__(self, tv):
+        self.tv = tv
+
+    def execute(self):
+        self.tv.turn_off()
+
+    def undo(self):
+        self.tv.turn_on()
+
+class ACOnCommand(Command):
+    def __init__(self, ac):
+        self.ac = ac
+
+    def execute(self):
+        self.ac.turn_on()
+
+    def undo(self):
+        self.ac.turn_off()
+
+class ACOffCommand(Command):
+    def __init__(self, ac):
+        self.ac = ac
+
+    def execute(self):
+        self.ac.turn_off()
+
+    def undo(self):
+        self.ac.turn_on()
+
+class MacroCommand(Command):
+    def __init__(self, commands):
+        self.commands = commands
+
+    def execute(self):
+        for command in self.commands:
+            command.execute()
+
+    def undo(self):
+        for command in reversed(self.commands):
+            command.undo()
+
 class RemoteControl:
     def __init__(self):
-        self.command_history = []
+        self.command_history = deque(maxlen=5)
 
-    def set_command(self, command):
+    def press_button(self, command):
+        command.execute()
         self.command_history.append(command)
-
-    def press_button(self):
-        if self.command_history:
-            self.command_history[-1].execute()
 
     def press_undo(self):
         if self.command_history:
-            self.command_history.pop().undo()
+            command = self.command_history.pop()
+            command.undo()
+        else:
+            print("История пуста, отменять нечего.")
+
+    def show_history(self):
+        print("\nИстория команд:")
+        for i, cmd in enumerate(self.command_history, 1):
+            print(f"{i}. {cmd.__class__.__name__}")
 
 light = Light()
+tv = TV()
+ac = AC()
+
 remote = RemoteControl()
 
-on_command = LightOnCommand(light)
-off_command = LightOffCommand(light)
+light_on = LightOnCommand(light)
+tv_on = TVOnCommand(tv)
+ac_on = ACOnCommand(ac)
 
-remote.set_command(on_command)
-remote.press_button()
+light_off = LightOffCommand(light)
+tv_off = TVOffCommand(tv)
+ac_off = ACOffCommand(ac)
 
+turn_on_all = MacroCommand([light_on, tv_on, ac_on])
+turn_off_all = MacroCommand([light_off, tv_off, ac_off])
+
+remote.press_button(light_on)
+remote.press_button(tv_on)
+remote.press_button(ac_on)
+
+remote.show_history()
+
+remote.press_undo()
+remote.show_history()
+
+remote.press_button(turn_on_all)
 remote.press_undo()
