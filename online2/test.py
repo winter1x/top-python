6832fba92b3e474610834a9bae9c536c57500869
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-09 22:23:45 +0500

    quick_sort pz done

--- online2/test.py
+++ online2/test.py
@@ -1,18 +1,37 @@
 """подсчет перестановок и итераций"""
-def quick_sort(arr, low, high):
-    if low < high:
-        pivot_index = partition(arr, low, high)
-        quick_sort(arr, low, pivot_index - 1)
-        quick_sort(arr, pivot_index + 1, high)
+def quick_sort(arr, low, high, ascending=True, counters=None):
+    if counters is None:
+        counters = {"iterations": 0, 'swaps': 0}
 
-def partition(arr, low, high, ascending=True):
+    if low < high:
+        pivot_index = partition(arr, low, high, ascending, counters)
+
+        quick_sort(arr, low, pivot_index - 1, ascending, counters)
+        quick_sort(arr, pivot_index + 1, high, ascending, counters)
+
+    return counters
+
+def partition(arr, low, high, ascending, counters):
     pivot = arr[high]
     i = low - 1
 
     for j in range(low, high):
+        counters['iterations'] += 1
         if (ascending and arr[j] <= pivot) or (not ascending and arr[j] >= pivot):
             i += 1
-            arr[i], arr[j] = arr[j], arr[i]
+            if i != j:
+                arr[i], arr[j] = arr[j], arr[i]
+                counters['swaps'] += 1
 
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    if (i + 1) != high:
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        counters['swaps'] += 1
+
     return i + 1
+
+numbers = list(map(int, input().split()))
+order = input()
+ascending = order == 'asc'
+counters = quick_sort(numbers, 0, len(numbers) - 1, ascending)
+
+print(numbers, counters['iterations'], counters['swaps'], sep='\n')
