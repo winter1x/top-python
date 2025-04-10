51c97179d11874e2b0f5272cbab68da76a8ac6c4
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-10 19:50:33 +0500

    adapter pz

--- online/test.py
+++ online/test.py
@@ -8,3 +8,8 @@ NewPaymentProcessor
 NewPaymentAdapter
 process_payment(process, amount)
 """
+
+old = OldPaymentProcessor()
+new = NewPaymentProcessor()
+process_payment(old, 150.0)
+process_payment(new, 150.0)
