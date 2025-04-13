c3acc210f138a0ddc02babef499d35cc9121d526
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-13 12:33:39 +0500

    decorator pz done

--- online/test.py
+++ online/test.py
@@ -10,3 +10,37 @@ PlainText - просто текст
     курсивdecorator *текст*
     зачеркнутdecorator ~~текст~~
 """
+
+class TextComponent:
+    def render(self) -> str:
+        raise NotImplementedError
+
+class PlainText(TextComponent):
+    def __init__(self, text: str):
+        self.text= text
+
+    def render(self) -> str:
+        return self.text
+
+class TextDecorator(TextComponent):
+    def __init__(self, component: TextComponent):
+        self._component = component
+
+    def render(self) -> str:
+        return self._component.render()
+
+class BoldDecorator(TextDecorator):
+    def render(self) -> str:
+        return f"**{super().render()}**"
+
+class ItalicDecorator(TextDecorator):
+    def render(self) -> str:
+        return f"*{super().render()}*"
+
+class StrikethroughDecorator(TextDecorator):
+    def render(self) -> str:
+        return f"~~{super().render()}~~"
+
+base = PlainText("hi")
+decorated = ItalicDecorator(BoldDecorator(StrikethroughDecorator(base)))
+print(decorated.render())
