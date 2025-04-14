2c4103aed0062b8f9ce965595e46e0e95d989a78
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-14 22:55:35 +0500

    pz наследование done

--- online2/test.py
+++ online2/test.py
@@ -9,3 +9,53 @@
  ■ Show — отображает имя животного;
  ■ Type — отображает название его подвида;
 """
+
+class Pet:
+    def __init__(self, name, characteristic):
+        self.name = name
+        self.characteristic = characteristic
+
+    def sound(self):
+        print('издает звук')
+
+    def show(self):
+        print(f"имя {self.name}")
+
+    def type(self):
+        print("Домашнее животное")
+
+class Dog(Pet):
+    def sound(self):
+        print("гав")
+
+    def type(self):
+        print('собака')
+
+class Cat(Pet):
+    def sound(self):
+        print('мяу')
+
+    def type(self):
+        print("кошка")
+
+class Parrot(Pet):
+    def sound(self):
+        print('чирик')
+
+    def type(self):
+        print("попугай")
+
+class Hamster(Pet):
+    def sound(self):
+        print('хомяк')
+
+    def type(self):
+        print("хомяк")
+
+
+dog = Dog("шарик", "овчарка")
+cat = Cat("мурка", "сиамская")
+
+for pet in [dog, cat]:
+    pet.show()
+    pet.type()
