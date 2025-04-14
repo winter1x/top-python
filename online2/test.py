492a1e71ce4a08bf2226fa2b369eb395badf0b7d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-14 22:33:56 +0500

    pz done

--- online2/test.py
+++ online2/test.py
@@ -6,3 +6,34 @@ Transport
 Car
 Bike
 """
+class Transport:
+    def __init__(self, speed):
+        self.speed = speed
+
+    def move(self):
+        print(f"движемся {self.speed} ", end='')
+
+class Car(Transport):
+    def __init__(self, speed, engine):
+        super().__init__(speed)
+        self.engine = engine
+
+    def move(self):
+        super().move()
+        print(f"с {self.engine}")
+
+class Bike(Transport):
+    def __init__(self, speed, pedals):
+        super().__init__(speed)
+        self.pedals = pedals
+
+    def move(self):
+        super().move()
+        print(f"с {self.pedals}")
+
+
+car = Car(100, 'v6')
+bike = Bike(120, 'standart pedals')
+
+car.move()
+bike.move()
