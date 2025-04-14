0ff44bef739caa7bb2af16f20a3e4c372caf42d9
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-14 22:34:38 +0500

    pz наследование

--- online2/test.py
+++ online2/test.py
@@ -1,39 +1,11 @@
 """
-реализовать систему управления транспортом, подумать над общими и уникальными методами/атрибутами, использовать наследование
-
-Transport
-
-Car
-Bike
+Создать базовыйкласс«Домашнееживотное»ипро
+изводныеклассы«Собака»,«Кошка»,«Попугай»,«Хомяк».
+Спомощью конструктора установить имя каждого жи
+вотного и его характеристики. Реализуйте для каждого
+из классов методы:
+■ Sound — издает звук животного (пишем текстом в
+консоль);
+ ■ Show — отображает имя животного;
+ ■ Type — отображает название его подвида;
 """
-class Transport:
-    def __init__(self, speed):
-        self.speed = speed
-
-    def move(self):
-        print(f"движемся {self.speed} ", end='')
-
-class Car(Transport):
-    def __init__(self, speed, engine):
-        super().__init__(speed)
-        self.engine = engine
-
-    def move(self):
-        super().move()
-        print(f"с {self.engine}")
-
-class Bike(Transport):
-    def __init__(self, speed, pedals):
-        super().__init__(speed)
-        self.pedals = pedals
-
-    def move(self):
-        super().move()
-        print(f"с {self.pedals}")
-
-
-car = Car(100, 'v6')
-bike = Bike(120, 'standart pedals')
-
-car.move()
-bike.move()
