17ae098b81537a8dcd0558d275006c81c9aa60c4
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-20 12:29:12 +0500

    tcp file + filename

--- offline/client.py
+++ offline/client.py
@@ -1,9 +1,15 @@
 import socket
+import os
+
+filename = 'отправляемый_файл.txt'
+filesize = os.path.getsize(filename)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('127.0.0.1', 9000))
 
-with open('отправляемый_файл.txt', 'rb') as f:
+client_socket.send(f"{filename}\n".encode())
+
+with open(filename, 'rb') as f:
     while chunk := f.read(1024):
         client_socket.send(chunk)
 
--- offline/server.py
+++ offline/server.py
@@ -1,13 +1,26 @@
 import socket
 
+def read_line(conn):
+    line = b''
+    while True:
+        char = conn.recv(1)
+        if char == b'\n' or not char:
+            break
+        line += char
+    return line.decode()
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('127.0.0.1', 9000))
 server_socket.listen(1)
 print('ожидание подключения')
+
 conn, addr = server_socket.accept()
 print(f'подключился клиент {addr}')
 
-with open('полученный_файл.txt', 'wb') as f:
+filename = read_line(conn)
+print(f'будем сохранять как {filename}')
+
+with open(filename, 'wb') as f:
     while True:
         data = conn.recv(1024)
         if not data:
