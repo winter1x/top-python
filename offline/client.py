c2d6902fd1bb5c55097c7cd3cb9a804a2b97b608
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-26 12:48:51 +0500

    udp

--- offline/client.py
+++ offline/client.py
@@ -1,10 +1,29 @@
 import socket
+import time
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.settimeout(3)
 
+server_address = ('localhost', 12345)
 message = 'привет сервер'
-client_socket.sendto(message.encode(), ('localhost', 12345))
+max_attempts = 3
 
-data, server_address = client_socket.recvfrom(1024)
+for attempt in range(1, max_attempts + 1):
+    try:
+        print(f"попытка {attempt}: отправка")
+        client_socket.sendto(message.encode(), server_address)
 
-print(f'ответ от сервера: {data.decode()}')
+        data, _ = client_socket.recvfrom(1024)
+        if data.decode() == 'ACK':
+            print("подтвердили")
+            break
+
+    except socket.timeout:
+        print("ответ от сервера не получен, пробуем снова")
+
+    except Exception as e:
+        print(f"ошибка клиента {e}")
+        break
+
+else:
+    print('сервер не ответил после 3 попыток')
--- offline/server.py
+++ offline/server.py
@@ -1,12 +1,18 @@
 import socket
-
+from time import sleep
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('localhost', 12345))
 print('запущен и ждет сообщений')
 
 while True:
-    data, client_address = server_socket.recvfrom(1024)
-    print(f'получено {client_address}: {data.decode()}')
+    try:
+        data, client_address = server_socket.recvfrom(1024)
+        message = data.decode()
+        print(f'[{client_address}] => {message}')
 
-    response = 'сообщение получено'
-    server_socket.sendto(response.encode(), client_address)
+        ack = 'ACK'
+        #sleep(10) # для выбрасывания ошибки
+        server_socket.sendto(ack.encode(), client_address)
+
+    except Exception as e:
+        print(f"ошибка на сервере {e}")
