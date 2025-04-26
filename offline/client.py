10b21d41d62c4ea359fa562d8a683139add43d32
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-26 12:27:15 +0500

    udp

--- offline/client.py
+++ offline/client.py
@@ -1,14 +1,10 @@
 import socket
 
-hostname = 'google.com'
-ip_address = socket.gethostbyname(hostname)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((ip_address, 80))
-request = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
-client_socket.send(request.encode())
+message = 'привет сервер'
+client_socket.sendto(message.encode(), ('localhost', 12345))
 
-respone = client_socket.recv(4096)
-print("ответ сервера", respone.decode(errors='ignore'))
+data, server_address = client_socket.recvfrom(1024)
 
-client_socket.close()
+print(f'ответ от сервера: {data.decode()}')
--- offline/server.py
+++ offline/server.py
@@ -1,25 +1,12 @@
-'''
-клиент узнает ip адрес сервера по имени хоста (DNS)
-сервер выводит ip адрес каждого подключившего клиента
-сообщения передаются по TCP
-логируется ip и порт всех входящих соединений
-socket
-'''
-
 import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('localhost', 12345))
+print('запущен и ждет сообщений')
 
-server_socket.bind(("0.0.0.0", 9000))
-server_socket.listen(1)
-print('("0.0.0.0", 9000)')
+while True:
+    data, client_address = server_socket.recvfrom(1024)
+    print(f'получено {client_address}: {data.decode()}')
 
-conn, addr = server_socket.accept()
-print(f'ip {addr[0]} порт {addr[1]}')
-
-data = conn.recv(1024)
-print('получено', data.decode())
-conn.send('принято'.encode())
-
-conn.close()
-server_socket.close()
+    response = 'сообщение получено'
+    server_socket.sendto(response.encode(), client_address)
