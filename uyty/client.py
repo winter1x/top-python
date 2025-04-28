c2446a39ee868238dd26da702e0af6fa5b0c2919
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-28 17:20:32 +0500

    edited

--- uyty/client.py
+++ uyty/client.py
@@ -1,24 +1,17 @@
 import socket
 import os
 
-filename = "отправляемый_файл.txt"
+filename = 'отправляемый_файл.txt'
 filesize = os.path.getsize(filename)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    client_socket.connect(("127.0.0.1", 8888))
-except ConnectionRefusedError:
-    print("не удается подключиться к серверу")
+client_socket.connect(('127.0.0.1', 8888))
 
 client_socket.send(f"{filename}\n".encode())
 
-with open(filename, "rb") as f:
+with open(filename, 'rb') as f:
     while chunk := f.read(1024):
         client_socket.send(chunk)
 
-respone = client_socket.recv(1024).decode()
-print(respone)
-print("файл отправлен")
+print("Файл отправлен.")
 client_socket.close()
-
-
--- uyty/server.py
+++ uyty/server.py
@@ -1,32 +1,32 @@
 import socket
 
 def read_line(conn):
-    line = b""
+    line = b''
     while True:
         char = conn.recv(1)
-        if char == b"\n" or not char:
+        if char == b'\n' or not char:
             break
         line += char
     return line.decode()
 
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('127.0.0.1', 8888))
 server_socket.listen(1)
-print("Server started")
+print("Ожидание клиента...")
 
 conn, addr = server_socket.accept()
-print("Connection established", addr)
+print(f"Клиент подключился: {addr}")
 
 filename = read_line(conn)
-print("Requested file:", filename)
+print(f"Будем сохранять как: {filename}")
 
-with open(filename, "wb") as file:
+with open(f"2{filename}", 'wb') as f:
     while True:
         data = conn.recv(1024)
         if not data:
             break
-        file.write(data)
-conn.send('File received'.encode())
+        f.write(data)
+
+print("Файл успешно получен.")
 conn.close()
 server_socket.close()
