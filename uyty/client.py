014b142cbdead3b39d79feb58dce71ed1487038f
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-05-12 16:06:30 +0500

    udp2

--- uyty/client.py
+++ uyty/client.py
@@ -1,7 +1,34 @@
 import socket
+import time
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket.settimeout(3)
+
+server_address = ("localhost", 12345)
 message = 'Hello, server!'
-client_socket.sendto(message.encode(), ("localhost", 12345))
-data, server_address = client_socket.recvfrom(1024)
-print("Received:", data.decode())
+max_attempts = 3
+
+for attempt in range(1, max_attempts + 1):
+    try:
+        print(f"Попытка {attempt}: Отправка сообщения '{
+            message}' на сервер {server_address}")
+        client_socket.sendto(message.encode(), server_address)
+
+        data, _ = client_socket.recvfrom(1024)
+        if data.decode() == 'ACK':
+            print("Сообщение успешно доставлено!")
+            break
+        else:
+            print("Ошибка доставки сообщения.")
+
+    except socket.timeout:
+        print(f"Попытка {attempt}: Таймаут при отправке сообщения.")
+        print("Повторная попытка отправки...")
+
+    except Exception as e:
+        print(f"ошибка клиента: {e}")
+        break
+
+else:
+    print(f'сервер не ответил после {max_attempts} попыток')
+        
--- uyty/server.py
+++ uyty/server.py
@@ -4,8 +4,12 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(("localhost", 12345))
 print("Сервер запущен. Ожидание сообщений...")
 while True:
-    data, client_address = server_socket.recvfrom(1024)
-    print("Получено сообщение от", client_address, ":", data.decode())
+    try:
+        data, client_address = server_socket.recvfrom(1024)
+        message = data.decode()
+        print(f"Получено сообщение от {client_address}: {message}")
 
-    response = "Сообщение успешно получено"
-    server_socket.sendto(response.encode(), client_address)
+        ack = "ACK"
+        server_socket.sendto(ack.encode(), client_address)
+    except Exception as e:
+        print(f"Ошибка на сервере: {e}")
