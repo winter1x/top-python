58e5d79e8b4dfd8efd89048baa2a18c09eb368b7
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-05-21 22:34:24 +0500

    interpreter pz done + pz

--- online/interpreter.py
+++ online/interpreter.py
@@ -72,6 +72,8 @@ expr = Addition(Number(2), Multiplication(Number(3), Number(4)))
 result = expr.interpret()
 print(result)
 
+expr2 = Subtraction(Number(10), Addition(Number(2), Number(3)))
+
 operations = {
     "+": lambda x, y: x + y,
     "-": lambda x, y: x - y,
@@ -84,3 +86,82 @@ def interpret(expression: str) -> int:
     return operations[op](left, right)
 
 print(interpret("2 + 3"))
+
+"""
+interpreter
+
+ON - включить свет
+OFF
+STATUS - показать текущее состояние
+
+input
+"""
+
+from abc import ABC, abstractmethod
+
+#интерфейс команды
+
+class Command(ABC):
+    @abstractmethod
+    def interpret(self, context):
+        pass
+
+#команды
+class TurnOnCommand(Command):
+    def interpret(self, context):
+        context["light"] = "ON"
+        print("свет вкл")
+
+class TurnOffCommand(Command):
+    def interpret(self, context):
+        context["light"] = "OFF"
+        print("свет выкл")
+
+class StatucCommand(Command):
+    def interpret(self, context):
+        print(f"сейчас {context['light']}")
+
+class CommandFactory:
+    commands = {
+        'ON': TurnOnCommand,
+        'OFF': TurnOffCommand,
+        'STATUS': StatucCommand
+    }
+
+    @staticmethod
+    def get_command(command_name):
+        return CommandFactory.commands.get(command_name, None)
+
+def main():
+    context = {'light': 'OFF'}
+    while True:
+        command_name = input().strip().upper()
+        if command_name == 'EXIT':
+            print("выход")
+            break
+
+        command_class = CommandFactory.get_command(command_name)
+        if command_class:
+            command = command_class()
+            command.interpret(context)
+        else:
+            print("неверн ком")
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+пользователь может быть описан полями
+role: строка ('admin', 'user')
+active: логическое (true, false)
+
+фильтры описываются:
+"role == admin" 
+"active == true"
+"role == admin AND active == true"
+"role == user OR active == false"
+"""
+
+user1 = {'role': 'admin', 'active': True}
+user2 = {'role': 'user', 'active': False}
