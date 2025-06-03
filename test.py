517deb7db1b76146a1492a7603fc548ca9e150f8
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-06-03 20:02:29 +0500

    posgre

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-1
+import psycopg2
+
+try:
+    connection = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        dbname="postgres",
+        user="postgres",
+        password="admin"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT NOW();")
+    current_time = cursor.fetchone()
+    print("Current time in DB:", current_time)
+
+except psycopg2.Error as e:
+    print("Database error:", e)
+
+finally:
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+
+#Current time in DB: (datetime.datetime(2025, 6, 3, 20, 1, 50, 360059, tzinfo=datetime.timezone(datetime.timedelta(seconds=18000))),)
