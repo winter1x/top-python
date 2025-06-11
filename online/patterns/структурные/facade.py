442c5ac11f11aef40907c5e0b709497139580191
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-06-11 22:00:52 +0500

    facade pz система бронирования путешествий

--- online/patterns/структурные/facade.py
+++ online/patterns/структурные/facade.py
@@ -1,5 +1,7 @@
 """
 facade
+
+используем когда:
 сложная система
 разделяем клиентский код и внутренности
 api проще
@@ -9,14 +11,19 @@ api проще
 подсистемы
 фасад
 
-adapter
-proxy
-mediator
+adapter меняет интерфейс 
+proxy контролирует доступ 
+mediator управляет взаимодействием объектов
 
 os
 pandas numpy matplotlib
 flask
 subprocess.run()
+
+(-)
+скрывает важные возможности
+если их нужно найти/увидеть больше гибкости, все-равно лезем внутрь
+может превратить в бога-объект
 """
 
 class DVDPlayer:
@@ -79,3 +86,76 @@ cinema = HomeTheaterFacade(dvd, projector, audio)
 
 cinema.watch_movie()
 cinema.end_movie()
+
+
+"""
+facade
+онлайн-магазин
+
+компоненты:
+каталог товаров
+система заказов
+платежная система
+уведомления
+
+оформить заказ одним вызовом place_order()
+"""
+
+class Catalog:
+    def get_product(self, product_id):
+        print(f'[Catalog] получение информации о товаре {product_id}')
+        return {'id': product_id, 'name': 'Ноутбук', 'price': 50000}
+
+class OrderSystem:
+    def create_order(self, user_id, product):
+        print(f'[OrderSystem] создание заказа {product["name"]} для {user_id}')
+        return {'order_id': 123, 'user_id': user_id, 'product': product}
+
+class PaymentProcessor:
+    def pay(self, user_id, amount):
+        print(f'[PaymentProcessor] оплата заказа {user_id} за {amount}')
+        return True
+
+class NotificationService:
+    def send_confirmation(self, user_id, message):
+        print(f'[NotificationService] отправка уведомления {user_id} сообщение: {message}')
+
+class ShopFacade:
+    def __init__(self):
+        self.catalog = Catalog()
+        self.order_system = OrderSystem()
+        self.payment = PaymentProcessor()
+        self.notifier = NotificationService()
+
+    def place_order(self, user_id: int, product_id: int):
+        print(f'[ShopFacade] оформление заказа {user_id}')
+        product = self.catalog.get_product(product_id)
+        order = self.order_system.create_order(user_id, product)
+        if self.payment.pay(user_id, product['price']):
+            self.notifier.send_confirmation(user_id, f'ваш заказ №{order["order_id"]} оформлен')
+        else:
+            print('[ShopFacade] Ошибка оплаты')
+        print('[ShopFacade] Заказ оформлен')
+
+
+
+
+shop = ShopFacade()
+shop.place_order(1, 1)
+
+
+"""
+система бронирования путешествий
+подсистемы:
+    бронирование авиабилетов
+    бронирование отеля
+    бронирование автобуса
+    стравки
+
+FlightBooking book_flight()
+HotelBooking book_hotel()
+TransferService book_transfer()
+InsuranceService buy_insurance()
+
+TravelFacade: book_all()
+"""
