e931d7af5de7a52872570cc1e61ab9a5a9bedc4d
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-06-16 20:58:15 +0500

    pz proxy прокси-доступ к дорогим операциям - генерация отчетов

--- online/patterns/структурные/proxy.py
+++ online/patterns/структурные/proxy.py
@@ -2,9 +2,20 @@
 proxy
 суррогат или заместитель
 
+структура:
 subject - общий интерфейс
 realSubject - реальный объект
 proxy - заместитель
+
++
+не нарушает принцип открытости/закрытости
+расширяемость
+ленивая инициализация
+
+-
+новый уровень косвенности
+может запутать, если сильно отличается
+нужно соблюдать интерфейс
 
 decorator
 """
@@ -54,3 +65,60 @@ doc = Document()
 proxy = SecureDocumentProxy(doc, 'guest')
 proxy.delete()
 
+"""
+клиент обращается к бд через прокси
+прокси проверяет авторизован ли, далее к реальному объекту
+
+DataServer
+SecureProxy
+Client
+"""
+
+class DataServerInterface:
+    def get_data(self):
+        pass
+
+class DataServer(DataServerInterface):
+    def get_data(self):
+        print('секретные данные получены')
+
+class SecureProxy(DataServerInterface):
+    _USERS = {
+        'admin': 'admin',
+        'editor': 'editor',
+    }
+
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+        self._real_server = None
+
+    def _authenticate(self):
+        return self._USERS.get(self._username) == self._password
+
+    def get_data(self):
+        if not self._authenticate():
+            return 'нет доступа'
+        if self._real_server is None:
+            self._real_server = DataServer()
+        return self._real_server.get_data()
+
+proxy1 = SecureProxy('admin', 'admin')
+print(proxy1.get_data())
+
+proxy2 = SecureProxy('guest', 'guest')
+print(proxy2.get_data())
+
+proxy3 = SecureProxy('editor', 'editor2')
+print(proxy3.get_data())
+
+"""
+прокси-доступ к дорогим операциям - генерация отчетов
+RealReport - долгая инициализация/дорогой
+    display()
+
+ReportProxy реализует тот же интерфейс, создает настоящий отчет только при первом вызове display()
+
+Report - интерфейс
+    display()
+"""
