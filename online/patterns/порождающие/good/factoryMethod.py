4889e17bb92571038c7738cce211baaa1872e9e8
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-06-18 20:48:53 +0500

    pz factoryMethod создание пользовательских уведомлений done

--- online/patterns/порождающие/good/factoryMethod.py
+++ online/patterns/порождающие/good/factoryMethod.py
@@ -58,5 +58,75 @@ def client_code(logistics: Logistics):
 
 client_code(RoadLogistics())
 client_code(SeaLogistics())
+print('#------------------------------------------------------------------------------')
 
 
+"""
+factoryMethod
+создание пользовательских уведомлений
+типы пользователей:
+гость - простые текстовые уведомления
+зарегистрированный пользователь - текстовое и фото уведомления
+администратор - доп метка внимание (логирование)
+
+Notification - абстрактный класс уведомления send(message)
+GuestNotification - простой текстовый уведомление
+UserNotification - текстовое и фото уведомление
+AdminNotification - текстовое и фото уведомление + доп метка внимание + log
+
+User create_notification - абстрактный фабричный метод
+Guest RegisteredUser Admin - реализация фабричного метода, возвращают соответсвующие уведомления
+
+клиентский код
+"""
+
+#from abc import ABC, abstractmethod
+
+class Notification(ABC):
+    @abstractmethod
+    def send(self, message):
+        pass
+
+class GuestNotification(Notification):
+    def send(self, message):
+        print(f"Guest: {message}")
+
+class UserNotification(Notification):
+    def send(self, message):
+        print(f"User: {message}")
+
+class AdminNotification(Notification):
+    log = []
+
+    def send(self, message):
+        full_message = f"Admin: внимание {message}"
+        print(full_message)
+        AdminNotification.log.append(full_message)
+
+class User(ABC):
+    @abstractmethod
+    def create_notification(self) -> Notification:
+        pass
+
+class Guest(User):
+    def create_notification(self) -> Notification:
+        return GuestNotification()
+
+class RegisteredUser(User):
+    def create_notification(self) -> Notification:
+        return UserNotification()
+
+class Admin(User):
+    def create_notification(self) -> Notification:
+        return AdminNotification()
+
+users = [Guest(), RegisteredUser(), Admin()]
+
+messages = ["Привет", "Сообщение", "Внимание!"]
+
+for user in users:
+    notifier = user.create_notification()
+    for message in messages:
+        notifier.send(message)
+        
+print(AdminNotification.log)
