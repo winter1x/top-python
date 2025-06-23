02c2fb517eebf51b8884cc8c7bbfa4a2e01a3827
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-06-23 21:04:48 +0500

    first tests

--- tests/tests/test_capitalize.py
+++ tests/tests/test_capitalize.py
@@ -1,5 +1,13 @@
 from src.capitalize import capitalize
 
+if capitalize("hello") != "Hello":
+    raise Exception("hello is not capitalized")
+
+if capitalize("") != "":
+    raise Exception("empty string is capitalized")
+
+if capitalize(None) != None:
+    raise Exception("None is capitalized")
 
 assert capitalize("hello") == "Hello"
 assert capitalize("") == ""
