95d949f10235ebc05f15c87815af54f28aa1cb20
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-06-30 21:08:43 +0500

    second tcp клиент - серверная

--- offline/сетевое/client1.py
+++ offline/сетевое/client1.py
@@ -1,24 +1,3 @@
-"""
-server1.py
-import socket
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 8888))
-
-server_socket.listen(1)
-print("Server is listening...")
-
-conn, addr = server_socket.accept()
-print(f"Connection from {addr}")
-
-data = conn.recv(1024)
-print(f"Received data: {data.decode()}")
-
-conn.send("Server received your data!".encode())
-
-conn.close()
-server_socket.close()
-"""
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- offline/сетевое/client2.py
+++ offline/сетевое/client2.py
@@ -1,26 +1,16 @@
 import socket
-import threading
-
-def receive_messages(sock):
-    while True:
-        try:
-            msg = sock.recv(1024)
-            if not msg:
-                break
-            print(">>", msg.decode())
-        except:
-            break
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 9000))
-
-recv_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-recv_thread.start()
+client_socket.connect(('127.0.0.1', 8888))
 
 while True:
-    msg = input()
-    if msg.lower() == 'выход':
-        break
+    msg = input("Введите сообщение: ")
     client_socket.send(msg.encode())
+
+    data = client_socket.recv(1024)
+    print("сервер:", data.decode())
+
+    if msg.lower() == "exit":
+        break
 
 client_socket.close()
