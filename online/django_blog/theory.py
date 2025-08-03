f87351bb31148812abcfbaeb9d893a72116e5ae5
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-08-03 12:22:36 +0500

    django-blog

---
+++ online/django_blog/theory.py
@@ -0,0 +1,18 @@
+"""
+uv init django_blog
+cd django_blog
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+uv add django
+uv run django-admin startproject django_blog .
+uv run django-admin version
+uv run manage.py runserver
+uv run manage.py migrate
+
+runserver - запуск сервера ращработки
+migrate - применение миграций
+makemigrations - создание миграций 
+shell - работа с базой через интерактивную оболочку
+createsuperuser - создание суперпользователя для админки
+test - запуск тестов
+startapp - генерация приложений
+"""
