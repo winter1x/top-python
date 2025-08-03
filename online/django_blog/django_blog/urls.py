dd186a95ed1e1f0da6d2e2c301c9b40c0ad83e83
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-08-03 12:40:14 +0500

    запрос-ответ

--- online/django_blog/django_blog/urls.py
+++ online/django_blog/django_blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_blog import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('about/', views.about),
 ]
