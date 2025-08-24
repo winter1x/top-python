38faa7e71e7b55e35e9446787ac9f100361ee1fb
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-08-24 14:10:41 +0500

    test

--- online/django_blog/django_blog/users/tests.py
+++ online/django_blog/django_blog/users/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class UsersTest(TestCase):
+    def test_users_list(self):
+        response = self.client.get(reverse('users'))
+        self.assertEqual(response.status_code, 200)
--- online/django_blog/django_blog/users/urls.py
+++ online/django_blog/django_blog/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.users_view),
+    path('', views.users_view, name='users'),
     #path('<int:user_id>/pets/<int:pet_id>/med_info/', views.pet_med_info_view),
     path(
         '<int:user_id>/pets/<int:pet_id>/med_info/',
