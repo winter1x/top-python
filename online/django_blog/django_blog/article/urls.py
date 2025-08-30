ef981f9f66fd269abe5151662e6ce9322c6a81f7
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-08-30 17:33:20 +0500

    R - CRUD просмотр

--- online/django_blog/django_blog/article/urls.py
+++ online/django_blog/django_blog/article/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django_blog.article import views
-from django_blog.article.views import IndexView
+from django_blog.article.views import IndexView, ArticleView #, ArticleCommentsView
 #from .views import IndexView
 
 urlpatterns = [
     path("<str:tags>/<int:article_id>", views.index, name="article"),
-    path("", IndexView.as_view(), name="index"),
+    path("", IndexView.as_view(), name="article_index"),
+    path("<int:id>/", ArticleView.as_view(), name="article_view"),
 ]
+
+# urlpatterns += [
+#     path('<int:article_id>/comments/<int:id>', ArticleCommentsView.as_view(), name='article_comments'),
+# ]
--- online/django_blog/django_blog/article/views.py
+++ online/django_blog/django_blog/article/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
-from django.shortcuts import redirect
 from django.urls import reverse
 from django_blog.article.models import Article
 
@@ -16,5 +15,16 @@ class IndexView(View):
             "articles/index.html",
             context={
                 "articles": articles
+            },
+        )
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(
+            request,
+            "articles/show.html",
+            context={
+                "article": article
             },
         )
