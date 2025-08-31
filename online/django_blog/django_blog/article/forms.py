25bb87a3289f2f71c5b5ac2e50cfc0e11f12aa02
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-08-31 14:41:49 +0500

    ModelForm

--- online/django_blog/django_blog/article/forms.py
+++ online/django_blog/django_blog/article/forms.py
@@ -1,9 +1,23 @@
 from django import forms
-
-class ArticleCommentForm(forms.Form):
-    text = forms.CharField(label="Комментарий", max_length=200)
-
+from .models import Comment
 # class ArticleCommentForm(forms.Form):
-#     class Meta:
-#         model = Comment
-#         fields = ['text']
+#     text = forms.CharField(label="Комментарий", max_length=200)
+
+class ArticleCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
+        labels = {
+            'text': 'Комментарий',
+        }
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Текст комментария'}),
+        }
+
+    def clean_content(self):
+        content = self.cleaned_data['text']
+        if "спам" in content.lower():
+            raise forms.ValidationError(
+                "Нельзя использовать спам в комментариях!"
+            )
+        return content
--- online/django_blog/django_blog/article/views.py
+++ online/django_blog/django_blog/article/views.py
@@ -44,28 +44,45 @@ class ArticleCommentView(View):
         )
 
 class CommentCreateView(View):
+    # def post(self, request, *args, **kwargs):
+    #     article = get_object_or_404(Article, id=kwargs['article_id'])
+    #     form = ArticleCommentForm(request.POST)
+
+    #     if form.is_valid():
+    #         Comment.objects.create(
+    #             text=form.cleaned_data["text"],
+    #             article=article,
+    #             author=request.user
+    #         )
+    #         return redirect('article_comments', article_id=article.id)
+
+    #     return render(
+    #         request,
+    #         "articles/create.html",
+    #         context={
+    #             "article": article,
+    #             "form": form
+    #         },
+    #     )
+
+    # def get(self, request, *args, **kwargs):
+    #     article = get_object_or_404(Article, id=kwargs['article_id'])
+    #     form = ArticleCommentForm()
+    #     return render(request, "articles/comments.html", context={"article": article, "form": form})
+
     def post(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['article_id'])
         form = ArticleCommentForm(request.POST)
-
         if form.is_valid():
-            Comment.objects.create(
-                text=form.cleaned_data["text"],
-                article=article,
-                author=request.user
-            )
+            comment = form.save(commit=False)
+            #comment.text = check_for_spam(form.cleaned_data["text"])
+            comment.author = request.user
+            comment.article = article
+            comment.save()
             return redirect('article_comments', article_id=article.id)
-
-        return render(
-            request,
-            "articles/create.html",
-            context={
-                "article": article,
-                "form": form
-            },
-        )
+        return render(request, "articles/create.html", context={"form": form, "article": article})
 
     def get(self, request, *args, **kwargs):
         article = get_object_or_404(Article, id=kwargs['article_id'])
         form = ArticleCommentForm()
-        return render(request, "articles/comments.html", context={"article": article, "form": form})
+        return render(request, "articles/create.html", context={"form": form, "article": article})
